1f33b2b2cb227cf5e8f5c5a02ccd6cba1522f856
Nikhil Sarwara <nikhil4818.be22@chitkara.edu.in>
2024-04-14 09:46:16 +0530

    add swagger file #7

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-
+// Swagger UI
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 // Routes
 import ticketRoutes from "./routes/ticket.routes.js";
 
-
+const swaggerDocument = YAML.load("./swagger.yaml");
 const app = express();
 // for body parser
 app.use(express.json());
@@ -22,6 +24,12 @@ app.get("/", (req, res) => {
   res.send("HLO");
 });
 
+// Swagger UI
+app.use(
+  "/api/musical-octo-tickets/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 app.use("/api/musical-octo-tickets/ticket", ticketRoutes);
 //Routes
 export default app;
